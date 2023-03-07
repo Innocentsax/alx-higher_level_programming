8be5a052c97bc6a29c0e89f9af08ab017d0839d1
INNOCENT CHARLES UDO <innocentcharlesudo@gmail.com>
2023-03-07 07:16:39 +0100

    IF, ELIF AND ELSE STATEMENT IN PYTHON

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+if number < 0:
+    lastdigit = number % -10
+else:
+    lastdigit = number % 10
+if lastdigit > 5:
+    print("Last digit of {:d} is {:d} and is greater than 5"
+          .format(number, lastdigit))
+elif lastdigit < 6 and lastdigit != 0:
+    print("Last digit of {:d} is {:d} and is less than 6 and not 0"
+          .format(number, lastdigit))
+else:
+    print("Last digit of {:d} is 0 and is 0".format(number))
