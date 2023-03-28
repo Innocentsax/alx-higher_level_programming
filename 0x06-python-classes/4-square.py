1700c16b496c92c9caf65a1bce4f6978fa2b46a6
INNOCENT CHARLES UDO <innocentcharlesudo@gmail.com>
2023-03-28 12:31:10 +0100

    Classes in Python

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# 0-square.py by UDO INNOCENT
+# 0-square.py by Ehoneah Obed
 """A module that defines a square """
 
 
@@ -43,4 +43,3 @@ class Square:
         """
 
         return (self.__size ** 2)
-
