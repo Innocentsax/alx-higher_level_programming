1f3c2eb321553d2689de650e0d4627155a7801a6
innocentsax <innocentcharlesudo@gmail.com>
2023-04-03 23:22:10 +0100

    More Classes in Python

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,36 +1,47 @@
 #!/usr/bin/python3
-"""Solves the N-queens puzzle"""
-
+"""Solves the N-queens puzzle.
+Determines all possible solutions to placing N
+N non-attacking queens on an NxN chessboard.
+Example:
+    $ ./101-nqueens.py N
+N must be an integer greater than or equal to 4.
+Attributes:
+    boarder (list): A list of lists representing the chessboard.
+    solutions (list): A list of lists containing solutions.
+Solutions are represented in the format [[r, c], [r, c], [r, c], [r, c]]
+where `r` and `c` represent the row and column, respectively, where a
+queen must be placed on the chessboard.
+"""
 import sys
 
 
-def init_board(n):
+def init_boarder(n):
     """Initialize an `n`x`n` sized chessboard with 0's."""
-    board = []
-    [board.append([]) for i in range(n)]
-    [row.append(' ') for i in range(n) for row in board]
-    return (board)
+    boarder = []
+    [boarder.append([]) for i in range(n)]
+    [row.append(' ') for i in range(n) for row in boarder]
+    return (boarder)
 
 
-def board_deepcopy(board):
+def board_deepcopy(boarder):
     """Return a deepcopy of a chessboard."""
-    if isinstance(board, list):
-        return list(map(board_deepcopy, board))
-    return (board)
+    if isinstance(boarder, list):
+        return list(map(boarder_deepcopy, boarder))
+    return (boarder)
 
 
-def get_solution(board):
+def get_solution(boarder):
     """Return the list of lists representation of a solved chessboard."""
     solution = []
-    for r in range(len(board)):
-        for c in range(len(board)):
-            if board[r][c] == "Q":
+    for r in range(len(boarder)):
+        for c in range(len(boarder)):
+            if boarder[r][c] == "Q":
                 solution.append([r, c])
                 break
     return (solution)
 
 
-def xout(board, row, col):
+def xout(boarder, row, col):
     """X out spots on a chessboard.
     All spots where non-attacking queens can no
     longer be played are X-ed out.
@@ -40,48 +51,48 @@ def xout(board, row, col):
         col (int): The column where a queen was last played.
     """
     # X out all forward spots
-    for c in range(col + 1, len(board)):
-        board[row][c] = "x"
+    for c in range(col + 1, len(boarder)):
+        boarder[row][c] = "x"
     # X out all backwards spots
     for c in range(col - 1, -1, -1):
-        board[row][c] = "x"
+        boarder[row][c] = "x"
     # X out all spots below
-    for r in range(row + 1, len(board)):
-        board[r][col] = "x"
+    for r in range(row + 1, len(boarder)):
+        boarder[r][col] = "x"
     # X out all spots above
     for r in range(row - 1, -1, -1):
-        board[r][col] = "x"
+        boarder[r][col] = "x"
     # X out all spots diagonally down to the right
     c = col + 1
-    for r in range(row + 1, len(board)):
-        if c >= len(board):
+    for r in range(row + 1, len(boarder)):
+        if c >= len(boarder):
             break
-        board[r][c] = "x"
+        boarder[r][c] = "x"
         c += 1
     # X out all spots diagonally up to the left
     c = col - 1
     for r in range(row - 1, -1, -1):
         if c < 0:
             break
-        board[r][c]
+        boarder[r][c]
         c -= 1
     # X out all spots diagonally up to the right
     c = col + 1
     for r in range(row - 1, -1, -1):
-        if c >= len(board):
+        if c >= len(boarder):
             break
-        board[r][c] = "x"
+        boarder[r][c] = "x"
         c += 1
     # X out all spots diagonally down to the left
     c = col - 1
-    for r in range(row + 1, len(board)):
+    for r in range(row + 1, len(boarder)):
         if c < 0:
             break
-        board[r][c] = "x"
+        boarder[r][c] = "x"
         c -= 1
 
 
-def recursive_solve(board, row, queens, solutions):
+def recursive_solve(boarder, row, queens, solutions):
     """Recursively solve an N-queens puzzle.
     Args:
         board (list): The current working chessboard.
@@ -91,16 +102,16 @@ def recursive_solve(board, row, queens, solutions):
     Returns:
         solutions
     """
-    if queens == len(board):
-        solutions.append(get_solution(board))
+    if queens == len(boarder):
+        solutions.append(get_solution(boarder))
         return (solutions)
 
-    for c in range(len(board)):
-        if board[row][c] == " ":
-            tmp_board = board_deepcopy(board)
-            tmp_board[row][c] = "Q"
-            xout(tmp_board, row, c)
-            solutions = recursive_solve(tmp_board, row + 1,
+    for c in range(len(boarder)):
+        if boarder[row][c] == " ":
+            tmp_boarder = boarder_deepcopy(boarder)
+            tmp_boarder[row][c] = "Q"
+            xout(tmp_boarder, row, c)
+            solutions = recursive_solve(tmp_boarder, row + 1,
                                         queens + 1, solutions)
 
     return (solutions)
@@ -117,7 +128,7 @@ if __name__ == "__main__":
         print("N must be at least 4")
         sys.exit(1)
 
-    board = init_board(int(sys.argv[1]))
-    solutions = recursive_solve(board, 0, 0, [])
+    boarder = init_boarder(int(sys.argv[1]))
+    solutions = recursive_solve(boarder, 0, 0, [])
     for sol in solutions:
         print(sol)
--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Defines a Rectangle class."""
+"""Display a Rectangle class."""
 
 
 class Rectangle:
-    """Display a rectangle.
+    """Structure of a rectangle.
     Attributes:
         number_of_instances (int): The number of Rectangle instances.
     """
@@ -63,20 +63,20 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return ("")
 
-        rect = []
+        rectx = []
         for i in range(self.__height):
-            [rect.append('#') for j in range(self.__width)]
+            [rectx.append('#') for j in range(self.__width)]
             if i != self.__height - 1:
-                rect.append("\n")
-        return ("".join(rect))
+                rectx.append("\n")
+        return ("".join(rectx))
 
     def __repr__(self):
         """Return the string representation of the Rectangle."""
-        rect = "Rectangle(" + str(self.__width)
-        rect += ", " + str(self.__height) + ")"
-        return (rect)
+        rectx = "Rectangle(" + str(self.__width)
+        rectx += ", " + str(self.__height) + ")"
+        return (rectx)
 
     def __del__(self):
         """Print a message for every deletion of a Rectangle."""
         type(self).number_of_instances -= 1
-        print("Bye rectangle..."
+        print("Bye rectangle...")
