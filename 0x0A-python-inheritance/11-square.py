b1a64fa8512b22142ebafe489daf6ed27e9bde29
INNOCENT CHARLES UDO <innocentcharlesudo@gmail.com>
2023-04-11 21:05:25 +0100

    Python Inheritance

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -9,8 +9,7 @@ class Square(Rectangle):
     def __init__(self, size):
         """Initialize a new square.
         Args:
-            size (int): The size of the new square 
-            size must be a positive interger.
+            size (int): The size of the new square.
         """
         self.integer_validator("size", size)
         super().__init__(size, size)
