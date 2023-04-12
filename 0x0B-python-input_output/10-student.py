d201b7eba0d2bcc1d1a2696aa7bd6ffb6486409d
INNOCENT CHARLES UDO <innocentcharlesudo@gmail.com>
2023-04-12 20:29:08 +0100

    Python Input/Output

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,24 +1,29 @@
 #!/usr/bin/python3
-"""defines a class Student"""
+"""Defines a class Student."""
 
 
 class Student:
     """Represent a student."""
 
     def __init__(self, first_name, last_name, age):
-        """Initializes a new Student
+        """Initialize a new Student.
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
         """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Get a dictionary representation of the Student
+        """Get a dictionary representation of the Student.
         If attrs is a list of strings, represents only those attributes
-        included in the list
+        included in the list.
+        Args:
+            attrs (list): (Optional) The attributes to represent.
         """
         if (type(attrs) == list and
                 all(type(ele) == str for ele in attrs)):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
-
