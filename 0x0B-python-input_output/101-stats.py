77381d7dedd71440536fe05e79ab92696cf25ada
INNOCENT CHARLES UDO <innocentcharlesudo@gmail.com>
2023-04-12 20:25:41 +0100

    Python Input/Output

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Reads from standard input and computes metrics.
+After every ten lines or the input of a keyboard interruption (CTRL + C),
 prints the following statistics:
     - Total file size up to that point.
     - Count of read status codes up to that point.
