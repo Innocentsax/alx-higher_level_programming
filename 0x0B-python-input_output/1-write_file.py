de7ab563b2cf33f6fccfa6b96162f81246574d06
INNOCENT CHARLES UDO <innocentcharlesudo@gmail.com>
2023-04-12 20:31:06 +0100

    Python Input/Output

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
-"""Write a function that writes a string to a text file (UTF8) and returns the number of characters written
-"""
+"""Defines a file-writing function."""
 
 
-def read_file(filename=""):
-    """Print the contents of a UTF8 text file to stdout."""
-    with open(filename, encoding="utf-8") as f:
-        print(f.read(), end="")
+def write_file(filename="", text=""):
+    """Write a string to a UTF8 text file.
+    Args:
+        filename (str): The name of the file to write.
+        text (str): The text to write to the file.
+    Returns:
+        The number of characters written.
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
