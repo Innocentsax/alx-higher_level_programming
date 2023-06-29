0d73451a5fc6f1569548f1f213640911076a528c
INNOCENT CHARLES UDO <innocentcharlesudo@gmail.com>
2023-06-29 22:13:44 +0100

    Update 1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
-i#!/bin/bash
-# Takes a URL, sends a GET request to the URL, and displays body of the response
-curl -Lsf "$1"
+#!/bin/bash
+# send a GET request to an URL with curl, and display the body of the response
+curl -sL "$1"
