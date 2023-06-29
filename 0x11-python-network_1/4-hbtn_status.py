987b1d019b028e70109dbd50da7bd23dfb220a34
INNOCENT CHARLES UDO <innocentcharlesudo@gmail.com>
2023-06-29 20:27:09 +0100

    Update 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,19 +1,10 @@
 #!/usr/bin/python3
-"""
-Module using request that fetches https://intranet.hbtn.io/status
-"""
+"""Fetches https://alx-intranet.hbtn.io/status."""
 import requests
 
 
-def main():
-    """
-    Function that fetches https://intranet.hbtn.io/status
-    """
-    url = 'https://intranet.hbtn.io/status'
-    r = requests.get(url)
-    print('Body response:')
-    print('\t- type: {}'.format(type(r.text)))
-    print('\t- content: {}'.format(r.text))
-
 if __name__ == "__main__":
-    main()
+    r = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
