7c23d7ca3c248b6a43ffb4b23b6388b5ce28d3fb
INNOCENT CHARLES UDO <innocentcharlesudo@gmail.com>
2023-07-25 21:22:15 +0100

    Update 101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,34 +1,20 @@
 #!/usr/bin/node
-
 const request = require('request');
-
-const movieId = process.argv[2];
-const url = `https://swapi.dev/api/films/${movieId}/`;
-let characters = [];
-
-request(url, (error, response, body) => {
-  if (error) {
-    console.log(error);
-    return;
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
   }
-
-  const data = JSON.parse(body);
-  characters = data.characters;
-  getCharacters(0);
 });
 
-const getCharacters = (index) => {
-  if (index === characters.length) {
-    return;
-  }
-
-  request(characters[index], (error, response, body) => {
-    if (error) {
-      console.log(error);
-      return;
+function printCharacters (characters, index) {
+  request(characters[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
+      }
     }
-    const characterData = JSON.parse(body);
-    console.log(characterData.name);
-    getCharacters(index + 1);
   });
-};
+}
